1055337348f0a008729f9ba69dc7231bf53df59b
Arthur40480 <arthurgibert@free.fr>
2022-11-17 11:57:43 +0100

    Add Cards components and finish Home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,34 @@
+import { useState } from "react";
+import PropTypes from "prop-types"
+import Lodgings from "../../logements.json";
+import { Link } from "react-router-dom";
 import Banner from "../../Components/Banner/Banner";
+import Card from "../../Components/Card/Card";
+import "./Home.css";
 
 function Home() {
-    return (
-        <Banner />
-    )
+  const [ lodgings ] = useState(Lodgings);
+
+  return (
+  <main className="home_main">
+    <Banner />
+      <section className="gallery">
+        { lodgings.map((lodging, id) =>
+            <Link to={`/logement/${lodging.id}` } key={id} className="card_link">
+              <Card cover={lodging.cover} title={lodging.title} />
+            </Link>
+          
+        )}    
+      </section>       
+  </main>
+  )
 }
+
+// Proptype permet de définir le type de chaques props
+Card.propTypes = {
+    key: PropTypes.string,
+    title: PropTypes.string,
+    cover: PropTypes.string,
+  }
 
 export default Home;
